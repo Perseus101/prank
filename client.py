bbae4ae2e4a637f323f75e990ee8dc8b7e5ade3c
Colin Moore <colin@moore.one>
2025-03-14 14:46:48 -0400

    Add soundboard features

--- client.py
+++ client.py
@@ -1,17 +1,102 @@
 import asyncio
 import websockets
+import traceback
 import json
+import os
+import glob
+import requests
 
 NAME = "colin-desktop"
+SERVER_URL = "localhost:8000"
+
+
+async def load_files(file_list):
+    os.makedirs("client_files", exist_ok=True)
+    files = []
+    files.extend(glob.glob("client_files/*.wav"))
+    files.extend(glob.glob("client_files/*.mp3"))
+    files = [f.replace("client_files/", "") for f in files]
+    for server_file in file_list:
+        if server_file in files:
+            continue
+        # Download file from server
+        response = requests.get(f"http://{SERVER_URL}/file/{server_file}")
+        if response.status_code != 200:
+            print(f"Failed to download file: {server_file}")
+            continue
+        with open(f"client_files/{server_file}", "wb") as f:
+            print(f"Downloaded file: {server_file}")
+            f.write(response.content)
+        files.append(server_file)
+
+    return files
+
+
+async def play_sound(filename):
+    process = await asyncio.create_subprocess_exec(
+        "aplay",
+        filename,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    if process.returncode != 0:
+        print(f"Failed to play sound: {filename}")
+        print(f"stdout: {stdout}")
+        print(f"stderr: {stderr}")
+
+
+class RestartNow(Exception):
+    pass
+
+
+async def client_main():
+    uri = f"ws://{SERVER_URL}/ws"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(json.dumps({"type": "id", "id": NAME}))
+        response = await websocket.recv()
+        response_json = json.loads(response)
+        assert response_json["type"] == "file_list"
+        files = await load_files(response_json["files"])
+
+        while True:
+            response = await websocket.recv()
+            response_json = json.loads(response)
+
+            if response_json["type"] == "restart":
+                raise RestartNow()
+            elif response_json["type"] == "file_list":
+                files = await load_files(response_json["files"])
+                print(f"Received updated file list: {files}")
+            elif response_json["type"] == "play_sound":
+                print(f"Playing sound: {response_json['filename']}")
+
+                assert (
+                    response_json["filename"] in files
+                ), f"File not found: {response_json['filename']}"
+
+                filename = os.path.join("client_files", response_json["filename"])
+
+                await play_sound(filename)
+            else:
+                print(f"Received from server: {response}")
 
 
 async def main():
-    uri = "ws://localhost:8000/ws"
-    async with websockets.connect(uri) as websocket:
-        await websocket.send(json.dumps({"type": "id", "id": NAME}))
-        while True:
-            response = await websocket.recv()
-            print(f"Received from server: {response}")
+    while True:
+        try:
+            await client_main()
+        except RestartNow:
+            print("Restarting")
+            continue
+        except KeyboardInterrupt:
+            break
+        except websockets.ConnectionClosedError:
+            print("Connection closed")
+        except Exception as e:
+            traceback.print_exc()
+
+        await asyncio.sleep(10)
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,15 +1,24 @@
 from sanic import Sanic, Request, Websocket
-from sanic.response import text, redirect
+from sanic.response import text, redirect, file
 from sanic_ext import render
 import json
 import asyncio
+import glob
+import os
 
 app = Sanic("App")
 
 
 @app.before_server_start
 async def setup(app, loop):
-    app.ctx.memory = {}
+    app.ctx.clients = {}
+
+    files = []
+
+    files.extend(glob.glob("server_files/*.wav"))
+    files.extend(glob.glob("server_files/*.mp3"))
+    files = [f.replace("server_files/", "") for f in files]
+    app.ctx.files = files
 
 
 @app.get("/")
@@ -17,15 +26,64 @@ async def index(request):
     error_msg = request.args.get("error_msg", None)
     return await render(
         "index.html",
-        context={"clients": app.ctx.memory.keys(), "error_msg": error_msg},
+        context={
+            "clients": app.ctx.clients.keys(),
+            "error_msg": error_msg,
+            "files": app.ctx.files,
+        },
         status=200,
     )
+
+
+@app.get("/soundboard/<client_name:str>")
+async def soundboard(request, client_name):
+    if client_name not in app.ctx.clients:
+        return redirect("/?error_msg=Client+not+found")
+    error_msg = request.args.get("error_msg", None)
+    return await render(
+        "soundboard.html",
+        context={
+            "client_name": client_name,
+            "files": app.ctx.files,
+            "error_msg": error_msg,
+        },
+        status=200,
+    )
+
+
+@app.post("/soundboard/<client_name:str>")
+async def play_sound(request, client_name):
+    if client_name not in app.ctx.clients:
+        return redirect("/?error_msg=Client+not+found")
+    file = request.form.get("file")
+    if file not in app.ctx.files:
+        return redirect(f"/soundboard/{client_name}?error_msg=File+not+found")
+    ws = app.ctx.clients[client_name]
+    await ws.send(json.dumps({"type": "play_sound", "filename": file}))
+    return redirect(f"/soundboard/{client_name}")
+
+
+@app.post("/soundboard/<client_name:str>/restart")
+async def restart_client(request: Request, client_name):
+    ws = app.ctx.clients.get(client_name)
+    if ws is None:
+        return redirect("/?error_msg=Client+not+found")
+    await ws.send(json.dumps({"type": "restart"}))
+    return redirect("/")
+
+
+@app.get("/file/<filename:str>")
+async def get_file(request, filename):
+    full_path = f"server_files/{filename}"
+    if not os.path.exists(full_path):
+        return text("File not found", status=404)
+    return await file(full_path, filename=filename)
 
 
 @app.post("/send/<client_id:str>")
 async def send_message(request: Request, client_id):
     print("Sending message to client", client_id)
-    ws = app.ctx.memory.get(client_id)
+    ws = app.ctx.clients.get(client_id)
     if ws is None:
         return redirect("/?error_msg=Client+not+found")
     await ws.send(json.dumps({"type": "server", "message": request.body.decode()}))
@@ -37,9 +95,10 @@ async def websocket_handler(request: Request, ws: Websocket):
     id_msg = await ws.recv()
     json_msg = json.loads(id_msg)
     assert json_msg["type"] == "id"
-    app.ctx.memory[json_msg["id"]] = ws
+    app.ctx.clients[json_msg["id"]] = ws
     print(f"Client with id {json_msg['id']} connected")
-    while True:
-        await asyncio.sleep(10)
-        await ws.ping()
-    print("Closed")
+    await ws.send(json.dumps({"type": "file_list", "files": app.ctx.files}))
+
+    await ws.auto_closer_task
+    print(f"Client with id {json_msg['id']} disconnected")
+    del app.ctx.clients[json_msg["id"]]
